a2052c32162257fa41d35c8b1da88df2a30cb8e9
Jeff Mandell <jeffamandell@gmail.com>
2026-07-31 01:51:59 -0400

    Add back in the obsidian plugin

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -8,19 +8,31 @@ return {
       },
     },
   },
-  -- obsidian.nvim
-  -- Comment out this plugin because it is annoying
-  --   {
-  --     "epwalsh/obsidian.nvim",
-  --     version = "*",
-  --     lazy = true,
-  --     ft = "markdown",
-  --     dependencies = { "nvim-lua/plenary.nvim" },
-  --     opts = {
-  --       workspaces = {
-  --         { name = "personal", path = "~/Jammin" }, -- change this path
-  --       },
-  --       ui = { enable = true },
-  --     },
-  --   },
+  {
+    "epwalsh/obsidian.nvim",
+    version = "*",
+    lazy = true,
+    ft = "markdown",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    opts = {
+      workspaces = {
+        { name = "personal", path = "~/Jammin" }, -- change this path
+      },
+      daily_notes = {
+        -- Optional, if you keep daily notes in a separate directory.
+        folder = "Daily",
+        -- Optional, if you want to change the date format for the ID of daily notes.
+        date_format = "%Y-%m-%d",
+        -- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
+        template = "DailyNoteFormatNVIM.md",
+      },
+      templates = {
+        folder = "Daily",
+        date_format = "%Y-%m-%d",
+        time_format = "%H:%M",
+      },
+
+      ui = { enable = true },
+    },
+  },
 }
